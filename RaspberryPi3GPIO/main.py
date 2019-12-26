e7a6d87c3a42136232718410aea48882e89ed316
LeandroTeodoroRJ <leandroteodoro.rj@gmail.com>
2019-12-25 23:59:54 -0300

    Atualizado Ex.GPIO

--- RaspberryPi3GPIO/main.py
+++ RaspberryPi3GPIO/main.py
@@ -1,6 +1,6 @@
-#************************************************************************
+#*******************************************************************
 #                  RASPBBERRRY PI 3 - GPIO
-#************************************************************************
+#*******************************************************************
 # -*- coding: utf-8 -*-
 '''
 Exemplo para manipulação das portas GPIO do Raspberry Pi3.
@@ -15,27 +15,37 @@ Interfaces -> Remote GPIO
 para verificar as mudanças das portas.
 6) A numeração em BCM é mostrada com o comando gpio readall.
 '''
-
+#************************* MÓDULOS ********************************
 import RPi.GPIO as GPIO   #Módulo de controle das portas GPIO
 import time               #Para uso de delay
+#******************************************************************
 
+#****************** CONSTANTES E VARIÁVEIS ************************
+LED = 16	#Saída para o led, pino 36 
+CHAVE = 12
+DELAY_TIME = 3
+#******************************************************************
+
+#******************** ENTRADAS E SAÍDAS ***************************
 GPIO.setmode(GPIO.BCM)    #Configura o GPIO para ser usado como portas paralelas
-GPIO.setup(16, GPIO.OUT)  #Configura a porta GPIO 16(pino 36) como saída
-GPIO.setup(12, GPIO.IN)   #Configura a porta GPIO 12 como entrada
+GPIO.setup(LED, GPIO.OUT)  #Configura a porta GPIO 16(pino 36) como saída
+GPIO.setup(CHAVE, GPIO.IN)   #Configura a porta GPIO 12 como entrada
+#******************************************************************
 
-for porta in range(0,3):  #loop
+#************************ MAIN CODE *******************************
+for porta in range(0,3):  #loop por 3 vezes
     print('Porta em nível alto')
-    GPIO.output(16, GPIO.HIGH)   
-    time.sleep(3)         #Delay 3 segundos
+    GPIO.output(LED, GPIO.HIGH)			#Coloca o pino em nível alto   
+    time.sleep(DELAY_TIME)         	#Delay 3 segundos
     print('Porta em nível baixo')
-    GPIO.output(16, GPIO.LOW)
-    time.sleep(3)
+    GPIO.output(LED, GPIO.LOW)			#Coloca o pino em nível baixo
+    time.sleep(DELAY_TIME)
     
-if (GPIO.input(12) == True):
+if (GPIO.input(CHAVE) == True):			#Testa a chave de entrada
     print('A porta de entrada esta em nível alto')
 else:
     print('A porta de entrada esta em nível baixo')
 
 GPIO.cleanup()  #Fecha a porta encerrando o processo.
 
-
+#******************************************************************
