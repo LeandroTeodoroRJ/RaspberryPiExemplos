eba4f5851202d2a00bb2f577b22fbbcdc223581c
Leandro Teodoro <leandroteodoro.rj@gmail.com>
2021-01-25 17:39:44 -0300

    Add files via upload

--- Interface_SPI/SPIHyperTerminal.py
+++ Interface_SPI/SPIHyperTerminal.py
@@ -2,25 +2,19 @@
 #                        HYPER TERMINAL SPI PARA RASPBERRY PI3 MODEL B+
 #*************************************************************************************************
 # Status: Em desenvolvimento
-
-#import tkinter as tk    #Importa a biblioteca tkinter e nomeia como tk
-#from tkinter import*
-#from tkinter.ttk import*
+'''
+Observações:
+1- Falta implementação da rotina de recebimento de dados.
+'''
 
 from tkinter import*
 from tkinter.ttk import*
 import time
-#import spidev
+import spidev
 
 #Funções
-#def action_open_window(event):
-    #ed_freq_clock.insert(1, '5000')
-#    pass
-
 def bt_brir_porta_click():
-#    spi = spidev.SpiDev()
     speed_clock = int(ed_freq_clock.get())       #Converte o valor digitado no edit para inteiro
-#   spi.max_speed_hz = speed_clock
     device = int(slave_dev.get())
     if (porta.get() == 'SPI0'):
         bus = 0
@@ -28,41 +22,44 @@ def bt_brir_porta_click():
         bus = 1
     else:
         bus = 2
-#   spi.open(bus, device)
+
+    spi.open(bus, device)
+    spi.max_speed_hz = speed_clock
+
     if ( idle.get() == 'Baixo' and aquisicao.get() == 'Idle para Ativo'):
-#       spi.mode = 0b00
-        print(00)
+       spi.mode = 0b00
     elif (idle.get() == 'Baixo' and aquisicao.get() == 'Ativo para Idle'):
-#       spi.mode = 0b01
-        print(1)
+       spi.mode = 0b01
     elif ( idle.get() == 'Alto' and aquisicao.get() == 'Idle para Ativo'):
-#       spi.mode = 0b10
-        print(10)
+       spi.mode = 0b10
     else:   #( idle.get() == 'Alto' and aquisicao.get() == 'Ativo para Idle')
-#       spi.mode = 0b11
-        print(11)
+       spi.mode = 0b11
+
 
 def bt_fechar_porta_click():
-#   spi.close()
-    pass
+    spi.close()
+
 
 def bt_enviar_click():
     delay = int(ed_delay.get())
     bytes_rx = int(ed_num_bytes_rx.get())
     send_string = tbox_enviado.get(1.0, END)
     to_send = list(map(int,send_string.split()))
-    print(to_send)
-#    spi.xfer(to_send)
-    time.sleep(delay/1000)      #Delay milisegundos
+#    print(to_send)
+    spi.xfer(to_send)
+#   time.sleep(delay/1000)      #Delay milisegundos
 #   lista_rx = spi.readbytes(bytes_rx)
 #   print(lista_rx)
+
+#Inicialização
+spi = spidev.SpiDev()
 
 #GUI
 janela = Tk()        #Instancia a variável janela como uma classe Tk
 janela.title('Hyper Terminal SPI')    #Título superior da janela
 
 #Configurações da janela
-janela.geometry('450x350+0+0')  # 'LARGURAxALTURA+DISTÂNCIA_ESQUERDA+DISTÂNCIA_DO_TOPO'
+janela.geometry('600x350+0+0')  # 'LARGURAxALTURA+DISTÂNCIA_ESQUERDA+DISTÂNCIA_DO_TOPO'
                                     #passado em formato string. Medidas em pixels.
 janela.resizable(height = False, width = False)  #Desabilita o remimensionamento da janela
 
@@ -76,8 +73,8 @@ lb_delay = Label(janela, text='Delay para recemimento de dados[ms]: ')
 lb_num_bytes_rx = Label(janela, text='Número de bytes que serão recebibos: ')
 lb_bytes_enviados = Label(janela, text='Bytes enviados: [Enviar em decimal separado por espaço]')
 lb_bytes_recebidos = Label(janela, text='Bytes recebibos: ')
-bt_abrir_porta = Button(janela, text='Abrir Porta SPI', width=35, command=bt_brir_porta_click)
-bt_fechar_porta = Button(janela, text='Fechar Porta SPI', width=35, command=bt_fechar_porta_click)
+bt_abrir_porta = Button(janela, text='Abrir Porta SPI', width=30, command=bt_brir_porta_click)
+bt_fechar_porta = Button(janela, text='Fechar Porta SPI', width=30, command=bt_fechar_porta_click)
 bt_enviar = Button(janela, text='Enviar', width=15, command=bt_enviar_click)
 ed_freq_clock = Entry(janela, width=32)
 ed_freq_clock.insert(0, '5000')
@@ -111,10 +108,10 @@ cb_slave.current(0)        #Define a primeira opção como default
 
 rolbar1 = Scrollbar(janela, orient=VERTICAL)   #Define a orientação vertical para a barra de rolagem
 rolbar2 = Scrollbar(janela, orient=VERTICAL)   #Define a orientação vertical para a barra de rolagem
-tbox_enviado = Text(janela, wrap=WORD, width=55, height=3, yscrollcommand=rolbar1.set)
+tbox_enviado = Text(janela, wrap=WORD, width=70, height=3, yscrollcommand=rolbar1.set)
 rolbar1.config(command=tbox_enviado.yview)   #Define que a barra de rolagem será anexada
                                                     #ao objeto Caixa_de_texto (text).
-tbox_recebido = Text(janela, wrap=WORD, width=55, height=3, yscrollcommand=rolbar2.set)
+tbox_recebido = Text(janela, wrap=WORD, width=70, height=3, yscrollcommand=rolbar2.set)
 rolbar2.config(command=tbox_recebido.yview)
 
 #Layout
@@ -139,10 +136,6 @@ tbox_enviado.grid(row=9, column=0, sticky=W, columnspan=2)
 bt_enviar.grid(row=10, column=0, sticky=W)
 lb_bytes_recebidos.grid(row=11, column=0, sticky=W)
 tbox_recebido.grid(row=12, column=0, sticky=W, columnspan=2)
-
-#Eventos
-#janela.bind("<Visibility>", action_open_window)             #Evento de fechar a aberta
-
 
 
 janela.mainloop()      #Abre a janela e interrompe o script enquanto a janela estiver sendo exibida
